0902856a06718df8b3115accf939e9b71a9e43b3
Kacper Orłowski <k.orlowski2002@gmail.com>
2026-08-18 22:58:38 +0200

    Add first EF Core migration, wire up AppDbContext with PostgresSQL

--- PropertyMaintenanceApi/Program.cs
+++ PropertyMaintenanceApi/Program.cs
@@ -1,7 +1,13 @@
+using PropertyMaintenanceApi;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
